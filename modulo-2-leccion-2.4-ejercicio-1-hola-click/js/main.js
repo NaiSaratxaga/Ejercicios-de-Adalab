09c59ecabbf9b130fb2c4f6dbf7d3c93d626ab7a
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-22 22:57:18 +0100

    new exercises

---
+++ modulo-2-leccion-2.4-ejercicio-1-hola-click/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const button = document.querySelector(".button");
+
+
+
+
+button.addEventListener("click", () => {
+  console.log("Mi primer click, ¡ole yo y la mujer que me parió!"); 
+});
