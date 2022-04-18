4105bd1ef9d82be493d17e715f0ed4df53f47a3a
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-18 22:29:16 +0200

    Clean up

--- Modulo-3/modulo-3-leccion-1-ejercicio-1/src/index.js
+++
@@ -1,6 +0,0 @@
-// Fichero src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
