a147603958f246c2c12fc49405888a0ab4c3bd95
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-18 23:31:44 +0200

    exercise 12.2

--- Modulo-2/modulo-2-lecciones-2.12-ejercicio-12-2-2-mi_tema_favorito/js/main.js
+++ Modulo-2/modulo-2-lecciones-2.12-ejercicio-12-2-2-mi_tema_favorito/js/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const colors = document.querySelectorAll('input[type=radio][name="color"]');
+
+function setColor(color) {
+  document.body.classList.remove('claro');
+  document.body.classList.remove('oscuro');
+  document.body.classList.add(color);
+
+  localStorage.setItem('color', color);
+}
+
+function onColorChange(event) {
+  const selectedColor = event.target.value;
+  setColor(selectedColor);
+}
+
+colors.forEach((color) => color.addEventListener('change', onColorChange));
+
+const savedColor = localStorage.getItem('color');
+if (savedColor) {
+  setColor(savedColor);
+  const colorRadioSaved = document.querySelector(
+    `input[type=radio][name="color"][value=${savedColor}]`
+  );
+  colorRadioSaved.checked = true;
+}
